71965b7fa420702c8e16f6d4ba95ad58e26b3c1f
JayCeD1 <jesseeren@gmail.com>
2023-09-06 18:23:19 +0300

    single page ng project

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {ServerService} from "./service/server.service";
+import {BehaviorSubject, catchError, map, Observable, of, startWith} from "rxjs";
+import {CustomResponse} from "./interface/custom-response";
+import {AppState} from "./interface/app-state";
+import {DataState} from "./enum/data-state.enum";
+import {Status} from "./enum/status.enum";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'serverapp';
+
+  appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  filterStatus$ = this.filterSubject.asObservable();
+
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void{
+    this.appState$ = this.serverService.servers$
+        .pipe(
+            map(response => {
+              return { dataState: DataState.LOADED_STATE, appData: response }
+            }),
+            startWith({ dataState: DataState.LOADING_STATE }),
+
+            catchError((error: string) => {
+              return of({ dataState: DataState.ERROR_STATE, error })
+            })
+        )
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {NgOptimizedImage} from "@angular/common";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NgOptimizedImage
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
